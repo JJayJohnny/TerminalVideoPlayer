7690b5da7edd0d2fddfd482208d7a47e2f51196d
JJayJohnny <jasiek.barczewski@gmail.com>
2023-07-14 12:05:38 +0200

    Added audio and video synchronization

--- player.py
+++ player.py
@@ -10,12 +10,14 @@ import numpy as np
 import sounddevice as sd
 
 FPS = True
+SYNNCHRONIZE_EVERY = 10
 
 class Player:
     def __init__(self, src: str):
         self.frameQueue = Queue()
         self.asciiQueue = Queue()
         self.decodedAudio = Queue()
+        self.synchronizationQueue = Queue()
 
         decoder = Thread(target=self.decode, args=(src,))
         decoder.start()
@@ -54,6 +56,7 @@ class Player:
         
         stop = time.time()
         os.system('clear')
+        frameCounter = 0
         while True:
             message = self.asciiQueue.get()
             if message == 'END':
@@ -64,10 +67,13 @@ class Player:
             if (time.time() - self.timeZero) < timeStamp:
                 time.sleep(max(timeStamp - (time.time()-self.timeZero), 0))
             print(Cursor.POS(1,1) + ascii)
+            if frameCounter%SYNNCHRONIZE_EVERY == 0:
+                self.synchronizationQueue.put(timeStamp)
             if FPS:
                 stop = time.time()
             print(Cursor.POS(1,1) + f"Frame time: {stop-start} FPS: {int(1.0/(stop-start))}")
             print(Cursor.POS(1,2) + str(round(timeStamp, 2)))
+            frameCounter += 1
 
 
     def decode(self, src: str):
@@ -96,15 +102,14 @@ class Player:
             for frame in packet.decode():
                 if type == 'video':
                     self.frameQueue.put((float(frame.pts*timeBase), frame.to_image()))
-                    # self.frameQueue.put(frame.to_image())
                 if type == 'audio':
                     self.decodedAudio.put((float(frame.pts*audioTimeBase), frame.to_ndarray().T))
-                    # self.decodedAudio.put(frame.to_ndarray().T)
         self.frameQueue.put('END')
         self.decodedAudio.put('END')
                 
     def playAudio(self):
         audioQueue = Queue()
+        correctionTime = 0.0
 
         if self.decodedAudio.get() != 'START':
             return
@@ -126,6 +131,11 @@ class Player:
                 if message == 'END':
                     break
                 timeStamp, audioFrame = message
-                if (time.time() - self.timeZero) < timeStamp:
-                    time.sleep(max(timeStamp - (time.time() - self.timeZero), 0))
+                try:
+                    videoTimeStamp = self.synchronizationQueue.get_nowait()
+                except Empty:
+                    videoTimeStamp = timeStamp
+                correctionTime += timeStamp-videoTimeStamp
+                if (time.time() - self.timeZero - correctionTime) < timeStamp:
+                    time.sleep(max(timeStamp - (time.time() - self.timeZero - correctionTime), 0))
                 audioQueue.put_nowait(audioFrame)
